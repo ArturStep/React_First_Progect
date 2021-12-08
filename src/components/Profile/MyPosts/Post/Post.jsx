d5234c903020bdbfb4445671709da8c9ec108e04
ArturStep <www.69988844@gmail.com>
2021-12-08 14:06:01 +0300

    feat: add props to Posts

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import s from './Post.module.css';
 
-const Post = () => {
+const Post = (props) => {
+
   return (
     <div className={s.item}>
       <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRL0EU78bTgk-k0C7TUTIXz1ilDTo2Pg6LT1A&usqp=CAU' />
-      post 1
+        { props.message }
           <div>
         <span>like</span>
       </div>
     </div>
   )
-
 }
 
 export default Post;
