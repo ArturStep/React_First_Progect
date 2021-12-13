4c3c2d759b10d120dd4b5fc43cee6ca89f095128
ArturStep <www.69988844@gmail.com>
2021-12-13 20:07:50 +0300

    feat: add dialogs message

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,9 +3,34 @@ import s from './Dialogs.module.css'
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' +s.active}>
+                    Artur
+                </div>
+                <div className={s.dialog}>
+                    Polina
+                </div>
+                <div className={s.dialog}>
+                    Artem
+                </div>
+                <div className={s.dialog}>
+                    Eugene
+                </div>
+                <div className={s.dialog}>
+                    Kristina
+                </div>
+                <div className={s.dialog}>
+                    Roma
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How is your it-kamasutra</div>
+                <div className={s.message}>Yo</div>
+            </div>
         </div>
+
     )
 }
 
