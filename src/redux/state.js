591f4bd9f73892b276dad4288ae86e7361878201
ArturStep <www.69988844@gmail.com>
2022-01-05 17:41:59 +0300

    feat: passing callback via props

--- src/redux/state.js
+++ src/redux/state.js
@@ -26,4 +26,15 @@ let state = {
     }
 }
 
-export default state
+export let addPost = (postMessage) => {
+    debugger;
+    let newPost = {
+        id: 5,
+        message: postMessage,
+        likesCount: 0
+    };
+
+    state.profilePage.posts.push(newPost);
+}
+
+export default state;
