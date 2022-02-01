bc7a3624e1510c17f0479e37d98e708e68aef3f3
ArturStep <www.69988844@gmail.com>
2022-02-01 18:30:38 +0300

    feat: add class components

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,6 +11,9 @@ const Navbar = () => {
             <div className={`${s.item} ${s.active}`}>
                 <NavLink to="/dialogs" className = { navData => navData.isActive ? s.active : s.item }>Messages</NavLink>
             </div>
+            <div className={`${s.item} ${s.active}`}>
+                <NavLink to="/users" className = { navData => navData.isActive ? s.active : s.item }>Users</NavLink>
+            </div>
             <div className={s.item}>
                 <NavLink to="/news" className = { navData => navData.isActive ? s.active : s.item }>News</NavLink>
             </div>
