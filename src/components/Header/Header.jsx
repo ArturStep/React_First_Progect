5550e48082864157e05148d69cde288363fe4945
ArturStep <www.69988844@gmail.com>
2022-02-06 20:17:34 +0300

    feat: add auth

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import s from './Header.module.css';
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
     return <header className={s.header}>
-        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQWXaDd7COVSlym_qmyXEr0nLSa34w31RN0g&usqp=CAU' />
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQWXaDd7COVSlym_qmyXEr0nLSa34w31RN0g&usqp=CAU'/>
+
+        <div className={s.loginBlock}>
+            {props.isAuth ? props.login
+                : <NavLink to={'/login'}>Login</NavLink>}
+        </div>
     </header>
 }
 
